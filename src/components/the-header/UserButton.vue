11ddbf49397de681c29f9d1a1d808df0e87bd27e
vladimir vychikov <vvvychikov@gmail.com>
2023-01-13 01:28:26 +0200

    AI-6: added header component to default layout

---
+++ src/components/the-header/UserButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="flex items-center rounded-lg border border-white p-2 space-x-3
+    cursor-pointer hover:shadow-1white transition duration-300"
+  >
+    <IconMenu />
+    <IconAvatar />
+  </div>
+</template>
