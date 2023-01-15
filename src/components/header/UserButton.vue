292c121fd768662cec05771025caf5587ab9e9b3
vladimir vychikov <vvvychikov@gmail.com>
2023-01-15 02:06:40 +0200

    fix: small header style changes

--- src/components/header/UserButton.vue
+++ src/components/header/UserButton.vue
@@ -1,9 +1,18 @@
 <template>
   <div
-    class="flex items-center border border-white p-2 space-x-3
-    cursor-pointer hover:shadow-1white transition duration-300"
+    class="header-button-hover"
   >
     <IconMenu />
     <IconAvatar />
   </div>
 </template>
+
+<style lang="scss">
+.header-button-hover{
+  &:hover svg{
+    fill: #000;
+  }
+  @apply flex items-center border border-white p-2 space-x-3
+    cursor-pointer transition duration-300 hover:bg-white
+}
+</style>
