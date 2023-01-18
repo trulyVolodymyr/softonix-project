4b85ead5c06584adf3c5793fb033ba55aa6a752b
vladimir vychikov <vvvychikov@gmail.com>
2023-01-18 13:22:39 +0200

    AI-12: place item component added

--- src/views/place-item/components/PlaceItemNumberOfGuests.vue
+++ src/views/place-item/components/PlaceItemNumberOfGuests.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="w-full">
+    <h3
+      class="mb-4"
+    >
+      This place has a maximum of {{ place.numberOfGuests }} guests
+    </h3>
     <div class="flex justify-between w-full mb-4">
       <div>
-        <p>Adults</p>
-        <p>Age +13</p>
+        <p class=" text-sm font-bold">Adults</p>
+        <p class=" text-sm ">Age +13</p>
       </div>
       <div>
-        <el-input-number v-model="adults" :min="0" :max="numberOfGuests" @change="handleChange" />
+        <el-input-number v-model="adults" :min="0" :max="adultsMax" @change="handleChange" />
       </div>
     </div>
     <div class="flex justify-between w-full mb-4">
       <div>
-        <p>Children</p>
-        <p>Ages 2–12</p>
+        <p class=" text-sm font-bold">Children</p>
+        <p class=" text-sm ">Ages 2–12</p>
       </div>
       <div>
-        <el-input-number v-model="children" :min="0" :max="numberOfGuests" @change="handleChange" />
+        <el-input-number v-model="children" :min="0" :max="childrenMax" @change="handleChange" />
       </div>
     </div>
     <div class="flex justify-between w-full">
       <div>
-        <p>Infants</p>
-        <p>Under 2</p>
+        <p class=" text-sm font-bold">Infants</p>
+        <p class=" text-sm ">Under 2</p>
       </div>
       <div>
-        <el-input-number v-model="infants" :min="0" :max="numberOfGuests" @change="handleChange" />
+        <el-input-number v-model="infants" :min="0" :max="infantsMax" @change="handleChange" />
       </div>
     </div>
   </div>
@@ -36,11 +41,28 @@ const children = ref(0)
 const infants = ref(0)
 
 const placeItemStore = usePlaceItemStore()
-const { place } = storeToRefs(placeItemStore)
+const { place, guests } = storeToRefs(placeItemStore)
 const numberOfGuests = ref<number>(place.value.numberOfGuests)
 
-function handleChange () {
-  numberOfGuests.value = numberOfGuests.value - 1
-  console.log(numberOfGuests.value)
+const adultsMax = ref(numberOfGuests.value)
+const childrenMax = ref(numberOfGuests.value)
+const infantsMax = ref(numberOfGuests.value)
+
+const handleChange = () => {
+  const sum = adults.value + children.value + infants.value
+  if (sum === numberOfGuests.value) {
+    adultsMax.value = adults.value
+    childrenMax.value = children.value
+    infantsMax.value = infants.value
+  } else {
+    adultsMax.value = numberOfGuests.value
+    childrenMax.value = numberOfGuests.value
+    infantsMax.value = numberOfGuests.value
+  }
+
+  guests.value.adults = adults.value
+  guests.value.children = children.value
+  guests.value.infants = infants.value
 }
+
 </script>
--- src/views/place-item/place-item.store.ts
+++ src/views/place-item/place-item.store.ts
@@ -173,7 +173,24 @@ export const usePlaceItemStore = defineStore('PlaceItemStore', () => {
     }
   )
 
+  const guests = ref<IGuests>({
+    adults: 1,
+    children: 0,
+    infants: 0
+  })
+
+  const dates = ref('')
+
+  const numberOfDays = computed(() => {
+    if (dates.value) {
+      return (new Date(dates.value[1]).getTime() - new Date(dates.value[0]).getTime()) / 1000 / 60 / 60 / 24
+    }
+  })
+
   return {
-    place
+    place,
+    guests,
+    dates,
+    numberOfDays
   }
 })
