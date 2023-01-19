0db961a7f0bcfead577ae14f71bb614f22dcedfe
vsorochynskyi <v.sorochynskyi@softonix.org>
2023-01-19 18:49:33 +0200

    some changes with dropdown styles

--- src/components/header/HeaderDropdown.vue
+++ src/components/header/HeaderDropdown.vue
@@ -1,20 +1,20 @@
 <template>
-  <el-dropdown trigger="click" :hide-on-click="false" class="header">
+  <el-dropdown trigger="click" :hide-on-click="false">
     <UserButton class="el-dropdown-link" />
 
     <template #dropdown>
       <el-dropdown-menu>
         <template v-if="accessToken">
-          <el-dropdown-item class="block text-center" @click="logout"><p>Logout</p></el-dropdown-item>
+          <el-dropdown-item @click="logout"><p>Logout</p></el-dropdown-item>
         </template>
 
         <template v-else>
           <el-dropdown-item>
-            <router-link class="w-full text-center" :to="{name:$routeNames.login}">Login</router-link>
+            <router-link :to="{name:$routeNames.login}">Login</router-link>
           </el-dropdown-item>
 
           <el-dropdown-item>
-            <router-link class="w-full text-center" :to="{name:$routeNames.signUp}">Register</router-link>
+            <router-link :to="{name:$routeNames.signUp}">Register</router-link>
           </el-dropdown-item>
         </template>
       </el-dropdown-menu>
