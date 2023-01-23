fcedd802e3140a3ede6ef665c60cfeb6ed1a3fc1
trulyVolodymyr <110303571+trulyVolodymyr@users.noreply.github.com>
2023-01-23 11:35:12 +0200

    fixed filters url computed logic

--- src/views/places/places.store.ts
+++ src/views/places/places.store.ts
@@ -1,10 +1,92 @@
+const filtersStore = useFiltersStore()
+
+const { max, min, priceRange, roomType, bedroms, beds, bathrooms, propertyType, amenities } = storeToRefs(filtersStore)
 export const usePlacesStore = defineStore('placesStore', () => {
   const places = ref<IPlace[]>([])
   const placesFiltered = ref<IPlace[]>([])
   const maxlength = ref<number>(0)
   const startFiltered = ref<number>(0)
   const endFiltered = ref<number>(19)
-  const url = ref('')
+  const url = computed(() => {
+    const urlArr = ['https://pcdokqjfsewijuqgscrk.supabase.co/rest/v1/places?select=*']
+
+    /// pricing
+    if (priceRange.value[0] !== min.value || priceRange.value[1] !== max.value) {
+      urlArr.push(`&pricing=gt.${priceRange.value[0]}`)
+      urlArr.push(`&pricing=lt.${priceRange.value[1]}`)
+    }
+
+    /// room type
+    if (roomType.value) {
+      urlArr.push(`&roomType=eq.${roomType.value}`)
+    }
+
+    /// bedrooms
+    if (bedroms.value < 6 && bedroms.value !== 0) {
+      urlArr.push(`&bedrooms=eq.${bedroms.value}`)
+    }
+    if (bedroms.value === 6) {
+      urlArr.push(`&bedrooms=gt.${bedroms.value - 1}`)
+    }
+
+    /// beds
+    if (beds.value < 6 && beds.value !== 0) {
+      urlArr.push(`&beds=eq.${beds.value}`)
+    }
+    if (beds.value === 6) {
+      urlArr.push(`&beds=gt.${beds.value - 1}`)
+    }
+
+    /// bathrooms
+    if (bathrooms.value < 6 && bathrooms.value !== 0) {
+      urlArr.push(`&bathrooms=eq.${bathrooms.value}`)
+    }
+    if (bathrooms.value === 6) {
+      urlArr.push(`&bathrooms=gt.${bathrooms.value - 1}`)
+    }
+
+    /// property type
+    if (propertyType.value) {
+      urlArr.push(`&propertyType=eq.${propertyType.value.toLowerCase()}`)
+    }
+
+    /// amenities
+    /// essentials
+    if (amenities.value.essentials.length) {
+      const addPlusToElems = computed(() => {
+        return amenities.value.essentials.map((el: string) => el.replace(' ', '+'))
+      })
+
+      urlArr.push(`&essentials=ov.%7B${addPlusToElems.value.join('%2C')}%7D`)
+    }
+    /// features
+    if (amenities.value.features.length) {
+      const addPlusToElems = computed(() => {
+        return amenities.value.features.map((el: string) => el.replace(' ', '+'))
+      })
+
+      urlArr.push(`&features=ov.%7B${addPlusToElems.value.join('%2C')}%7D`)
+    }
+    /// location
+    if (amenities.value.location.length) {
+      const addPlusToElems = computed(() => {
+        return amenities.value.location.map((el: string) => el.replace(' ', '+'))
+      })
+
+      urlArr.push(`&amenities_location=ov.%7B${addPlusToElems.value.join('%2C')}%7D`)
+    }
+    /// safety
+    if (amenities.value.safety.length) {
+      const addPlusToElems = computed(() => {
+        return amenities.value.safety.map((el: string) => el.replace(' ', '+'))
+      })
+
+      urlArr.push(`&safety=ov.%7B${addPlusToElems.value.join('%2C')}%7D`)
+    }
+
+    return urlArr.join('')
+  })
+
   const filteredLength = ref<number>(0)
 
   function getChank (range: string) {
