2f17c991df927ce319689d30e9b833bb97f98328
trulyVolodymyr <110303571+trulyVolodymyr@users.noreply.github.com>
2023-01-30 14:26:09 +0200

    deploy changes removed

--- src/composables/http/index.ts
+++ src/composables/http/index.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    autauthority: import.meta.env.VITE_API_URL,
     apikey: import.meta.env.VITE_API_KEY
   }
 })
