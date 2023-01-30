3dd20c0070c1f7feb1abd04926e2310059d3aebb
trulyVolodymyr <110303571+trulyVolodymyr@users.noreply.github.com>
2023-01-30 14:24:45 +0200

    deply changed

--- src/composables/http/index.ts
+++ src/composables/http/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
+    autauthority: import.meta.env.VITE_API_URL,
     apikey: import.meta.env.VITE_API_KEY
   }
 })
